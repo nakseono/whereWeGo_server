1e150188eff6433e677c69bed3661cc21fb8f699
NakSeoNo <nakseono@gmail.com>
2020-07-24 22:01:59 +0900

    add server skeleton

---
+++ controllers.js
@@ -0,0 +1,2 @@
+const { user } = require('./models');
+module.exports = {} // 로그인,회원가입 등의 인증 부여 로직 작성
---
+++ index.js
@@ -0,0 +1,12 @@
+// 서버 구동을 위한 모듈들과 라우터, 포트 번호 등을 정의하는 파일이다.
+// const express = require('express');
+require('./models');
+
+// const bodyParser = require('body-parser');
+// const session = require('express-session');
+
+const app = express();
+
+const port = 4000;
+
+app.use();
