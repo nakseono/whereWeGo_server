3e2b31e44018946a287ba038b82011b855cd07d9
NakSeoNo <nakseono@gmail.com>
2020-07-27 22:14:11 +0900

    add index extra

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 // 서버 구동을 위한 모듈들과 라우터, 포트 번호 등을 정의하는 파일이다.
 // const express = require('express');
+const express = require('express');
 require('./models');
 
-// const bodyParser = require('body-parser');
-// const session = require('express-session');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+
+const {
+  signInController,
+  signUpController,
+  userContoroller
+} = require('./controllers');
 
 const app = express();
 
 const port = 4000;
 
-app.use();
+app.use(
+  session({
+    secret: '@switzerland',
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));
+}
+
+app.use(bodyParser.json());
+
+// ? Optional : cors
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    method: ['GET', 'POST'],
+    credentials: true
+  })
+);
+
+app.get('/user', userContoroller);
+app.post('/signin', signInController);
+app.post('/signup', signUpController);
+
+module.exports = app;
