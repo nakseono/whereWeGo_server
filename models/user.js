0f5aea0642bebf4d1c7232ace6a23cb3796d1e58
NakSeoNo <nakseono@gmail.com>
2020-07-30 08:11:13 +0900

    add user skeleton

--- models/user.js
+++ models/user.js
@@ -1,1 +1,10 @@
-// user 테이블에 필요한 스키마 정의하는 파일
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define(
+    'user', {
+
+    },
+    {}
+  );
+  user.associate = function(models) {};
+  return user;
+}// user 테이블에 필요한 스키마 정의하는 파일
