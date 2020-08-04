cb8bbdd9ec4b83b3852928085be4557a652e18d2
NakSeoNo <nakseono@gmail.com>
2020-08-04 19:15:04 +0900

    add signIn, signUp

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 const express = require('express');
 require('./models');
 
-const app = express();
-app.use(express.json()); // body-parser와 같은 역할을 한다.
 const session = require('express-session');
+
+const app = express();
 
 const port = 4000;
 
+const { 
+  signUpController, 
+  signInController
+} = require('./controller/user');
+
+app.use(express.json()); // body-parser와 같은 역할을 한다.
+
 app.use(
   session({
-    secret: 'secretcode',
+    secret: 'secretCode',
     resave: false,
     saveUninitialized: true
   })
 );
 
-app.get('/', (req, res) => {
-  res.send('Hello World - server test');
-})
+app.post('/signUp', signUpController);
+app.post('/signIn', signInController);
 
 app.listen(port, () => {
   console.log(`server listen on ${port}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/signIn', (req, res) => {
+  res.send('this is signIn page')
+})
+
+app.get('/signUp', (req, res) => {
+  res.send('this is signUp page')
+})
