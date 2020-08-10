bf23a16c11380142a0274044263078bffb70919a
NakSeoNo <nakseono@gmail.com>
2020-08-10 19:53:46 +0900

    add cors, delete page message

--- app.js
+++ app.js
@@ -1,42 +1,36 @@
-const express = require('express');
-require('./models');
+const express = require("express");
+require("./models");
 
-const session = require('express-session');
+const session = require("express-session");
+const cors = require("cors");
 
 const app = express();
 
 const port = 4000;
 
-const { 
-  signUpController, 
-  signInController
-} = require('./controller/user');
+const { signUpController, signInController } = require("./controller/user");
 
 app.use(express.json()); // body-parser와 같은 역할을 한다.
 
 app.use(
   session({
-    secret: 'secretCode',
+    secret: "secretCode",
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
   })
 );
 
-app.post('/signUp', signUpController);
-app.post('/signIn', signInController);
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    method: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.post("/signUp", signUpController);
+app.post("/signIn", signInController);
 
 app.listen(port, () => {
   console.log(`server listen on ${port}`);
 });
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/signIn', (req, res) => {
-  res.send('this is signIn page')
-})
-
-app.get('/signUp', (req, res) => {
-  res.send('this is signUp page')
-})
--- controller/user.js
+++ controller/user.js
@@ -1,58 +1,58 @@
-const { user } = require('../models');
+const { user } = require("../models");
 
 module.exports = {
   signUpController: (req, res) => {
-    const {email, password, username} = req.body;
+    const { email, password, username } = req.body;
 
     user
       .findOrCreate({
         where: {
-          email: email
+          email: email,
         },
         defaults: {
           email: email,
           password: password,
-          username: username
-        }
+          username: username,
+        },
       })
 
       .then(async ([user, created]) => {
         if (!created) {
-          return res.status(409).send('사용중인 이메일 입니다.');
+          return res.status(409).send("사용중인 이메일 입니다.");
         }
         const data = await user.get({ plain: true });
-        res.status(201).send('회원가입 성공!');
+        res.status(201).send("회원가입 성공!");
       })
 
       .catch((err) => {
-        res.status(404).send('err')
-        console.log(err)
-      })
+        res.status(404).send("err");
+        console.log(err);
+      });
   },
 
-  signInController: (req,res) => {
-    const {email, password} = req.body;
+  signInController: (req, res) => {
+    const { email, password } = req.body;
     const sess = req.session;
 
     user
       .findOne({
         where: {
           email: email,
-          password: password
-        }
+          password: password,
+        },
       })
 
-      .then(data => {
-        if(!data) {
-          return res.status(404).send('존재하지 않는 유저입니다.')
+      .then((data) => {
+        if (!data) {
+          return res.status(404).send("존재하지 않는 유저입니다.");
         }
         sess.userid = data.id;
-        res.status(200).send('로그인 성공!')
+        res.status(200).send("로그인 성공!");
       })
-      
+
       .catch((err) => {
-        res.status(404).send('err')
-        console.log(err)
-      })
-  }
-}
+        res.status(404).send("err");
+        console.log(err);
+      });
+  },
+};
